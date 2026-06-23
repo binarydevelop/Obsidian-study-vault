1b21847ead483e911a8d59888cd276a1f545cba5
Tushar Roy <61935187+binarydevelop@users.noreply.github.com>
2026-06-23 21:54:46 +0530

    Time: 3 ms (58.79%), Space: 65.4 MB (43.59%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
 var maxProfit = function(nums) {
-    let minPrice = nums[0]
     let maxProfit = 0
-    for(let i = 1; i<nums.length; i++){
-        let profit = 0
+    let minPrice = nums[0]
+    for(let i = 1; i< nums.length; i++){
+        let profit = nums[i] - minPrice
         if(nums[i] < minPrice){
             minPrice = nums[i]
         }
-        profit = nums[i] - minPrice
-        maxProfit = Math.max(maxProfit, profit)
+    maxProfit = Math.max(maxProfit, profit)
     }
 
     return maxProfit
