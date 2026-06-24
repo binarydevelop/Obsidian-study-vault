19eb3397d470f4026218fb504ebbf3f729c339d1
Tushar Roy <61935187+binarydevelop@users.noreply.github.com>
2026-06-24 15:40:45 +0530

    Time: 3 ms (67.65%), Space: 59.8 MB (15.31%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let minLength = Infinity
+    let start = 0
+    let currentSum = 0
+    for(let end = 0; end < nums.length; end++){
+        currentSum += nums[end]
+        while(currentSum >= target){
+            minLength = Math.min(minLength,(end - start)+ 1)
+            currentSum -= nums[start]
+            start++
+        }
+    }
+    return minLength == Infinity ? 0 : minLength;
+};
