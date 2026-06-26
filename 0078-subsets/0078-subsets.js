f37a40221b19ca9218e719d02e8af3eae240ac4e
Tushar Roy <61935187+binarydevelop@users.noreply.github.com>
2026-06-26 07:10:58 +0530

    Time: 0 ms (100%), Space: 54.6 MB (72.71%) - LeetHub

---
+++ 0078-subsets/0078-subsets.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  const result = [];
+  const path = [];
+
+  function backtrack(index) {
+    if (index === nums.length) {
+      result.push([...path]);
+      return;
+    }
+
+    // Choice 1: skip nums[index]
+    backtrack(index + 1);
+
+    // Choice 2: take nums[index]
+    path.push(nums[index]);
+    backtrack(index + 1);
+    path.pop();
+  }
+
+  backtrack(0);
+  return result;
+};
