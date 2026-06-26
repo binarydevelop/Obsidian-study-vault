4defdf54b8ab5b11d4d8da1b6a7adb83e312f768
Tushar Roy <61935187+binarydevelop@users.noreply.github.com>
2026-06-26 17:14:31 +0530

    Time: 1 ms (75.82%), Space: 54 MB (70.21%) - LeetHub

---
+++ JavaScript/Medium/0022-generate-parentheses/0022-generate-parentheses-06-26-2026-17-14-27.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let results = []
+    let openCount = 0
+    let closeCount = 0  
+    function backTrack(current, openCount, closeCount){
+        if(current.length == 2 * n){
+            results.push(current)
+            return 
+        }
+        if(openCount < n){
+            backTrack(current+'(', openCount+1, closeCount);
+        }
+        if(closeCount < openCount){
+            backTrack(current+')', openCount, closeCount+1);
+        }
+    }
+
+    backTrack('', 0, 0)
+
+    return results
+};
