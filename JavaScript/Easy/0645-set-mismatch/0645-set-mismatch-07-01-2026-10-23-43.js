fc0e782bf5bfed6763e99fb7dc44ceabcd82e3a4
Tushar Roy <61935187+binarydevelop@users.noreply.github.com>
2026-07-01 10:23:45 +0530

    Time: 3 ms (93.01%), Space: 55.4 MB (100%) - LeetHub

---
+++ JavaScript/Easy/0645-set-mismatch/0645-set-mismatch-07-01-2026-10-23-43.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+    let missing = -1;
+    let duplicate = -1;
+
+    for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1;
+
+        if (nums[index] < 0) {
+            duplicate = Math.abs(nums[i]);
+        } else {
+            nums[index] *= -1;
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            missing = i + 1;
+        }
+    }
+
+    return [duplicate, missing];
+};
