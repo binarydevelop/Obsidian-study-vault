dc363c1f52ffc191b6be18d1c1366a7bbdfb1f9d
Tushar Roy <61935187+binarydevelop@users.noreply.github.com>
2026-07-01 05:48:25 +0530

    Time: 1 ms (91.48%), Space: 57.8 MB (55.01%) - LeetHub

---
+++ JavaScript/Easy/0724-find-pivot-index/0724-find-pivot-index-07-01-2026-05-48-24.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let total = nums.reduce((sum, num) => sum + num, 0)
+
+    let leftSum = 0
+
+    for(let i = 0; i< nums.length; i++ ){
+        let rightSum = total - leftSum - nums[i];
+
+        if(leftSum == rightSum){
+            return i;
+        }
+
+        leftSum += nums[i]
+    }
+
+    return -1
+};
+
+let nums = [1,7,3,6,5,6]
+
+console.log(pivotIndex(nums))
