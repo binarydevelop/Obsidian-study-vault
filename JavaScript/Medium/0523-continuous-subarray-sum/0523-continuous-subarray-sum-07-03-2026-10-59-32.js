67825085cbe24b1169e7a00af62854513ca33f5e
Tushar Roy <61935187+binarydevelop@users.noreply.github.com>
2026-07-03 10:59:33 +0530

    Time: 31 ms (41.57%), Space: 84.5 MB (26.69%) - LeetHub

---
+++ JavaScript/Medium/0523-continuous-subarray-sum/0523-continuous-subarray-sum-07-03-2026-10-59-32.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function(nums, k) {
+    let map = new Map();
+    let cumulativeSum = 0;
+
+    map.set(0, -1);
+
+    for (let i = 0; i < nums.length; i++) {
+        cumulativeSum += nums[i];
+
+        let remainder = cumulativeSum % k;
+
+        if (map.has(remainder)) {
+            if (i - map.get(remainder) >= 2) {
+                return true;
+            }
+        } else {
+            map.set(remainder, i);
+        }
+    }
+
+    return false;
+};
+
+let nums = [23, 2, 4, 6, 7];
+let k = 6;
+
+console.log(checkSubarraySum(nums, k)); // true
